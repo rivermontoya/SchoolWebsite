c453df67772285bf73959cd639e0b106003c690b
SilverSong3 <78618783+SilverSong3@users.noreply.github.com>
2023-02-05 20:09:25 -0500

    Added single location navbar menu js function(s)

---
+++ public/scripts/menu.js
@@ -0,0 +1,21 @@
+"use strict";
+/*    
+      JavaScript for menu
+      Author: Elisabeth Ryder
+      Date:   2/5/23
+
+      Filename: menu.js
+*/
+
+/*------- NAV BAR IMPLEMENTATION -------*/
+
+function loadMenu() {
+//get the ul element by its id
+var Menu = document.getElementById("addMenuLinks");
+//add all links to site as needed
+Menu.innerHTML = "<a href=\"index.html\">Home</a>";
+Menu.innerHTML += "<a href=\"services.html\">Services</a>";
+Menu.innerHTML += "<a href=\"contact.html\">Contact</a>";
+}
+
+//NOTE: The " character must be escaped for the script to work properly.
