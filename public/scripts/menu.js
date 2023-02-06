c44db6c5c79bf5dcb17248867fe144f38bd70d44
rivermontoya <55554291+rivermontoya@users.noreply.github.com>
2023-02-05 23:24:19 -0500

    Added non redundant footer in menu.js. Added favicon to pages and footer. Better centered, styled, and integrated load in animation to home page.

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -10,12 +10,20 @@
 /*------- NAV BAR IMPLEMENTATION -------*/
 
 function loadMenu() {
-//get the ul element by its id
-var Menu = document.getElementById("addMenuLinks");
-//add all links to site as needed
-Menu.innerHTML = "<a href=\"index.html\">Home</a>";
-Menu.innerHTML += "<a href=\"services.html\">Services</a>";
-Menu.innerHTML += "<a href=\"contact.html\">Contact</a>";
-}
+    //get the ul element by its id
+    var Menu = document.getElementById("addMenuLinks");
+    //add all links to site as needed
+    Menu.innerHTML = "<a href=\"index.html\">Home</a>";
+    Menu.innerHTML += "<a href=\"services.html\">Services</a>";
+    Menu.innerHTML += "<a href=\"contact.html\">Contact</a>";
 
-//NOTE: The " character must be escaped for the script to work properly.
+
+    //get p element by id for footer
+    var Footer = document.getElementById("addFooter");
+
+    //working on centering footer better... styled with footer img
+    //in main.css
+    Footer.innerHTML = "<img height=\"30vh;\" src=\"images/favicon.svg\" alt=\"favicon\">";
+    Footer.innerHTML += "<p>WebDevs</p>";
+}
+    //NOTE: The " character must be escaped for the script to work properly.
