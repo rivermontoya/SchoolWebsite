e023d66d06055a2a0445f3187deb01f2e0e54bba
SilverSong3 <78618783+SilverSong3@users.noreply.github.com>
2023-02-12 23:41:48 -0500

    Added serviceOptions array implementation

--- public/scripts/servicesArray.js
+++ public/scripts/servicesArray.js
@@ -7,3 +7,15 @@ let serviceItems = [
     "Testing",
     "Quality Assurance"
 ];
+
+serviceItems.forEach(addServices);
+
+function addServices(index) {
+    const serviceOptionsNode = document.getElementById("serviceOptions"); 
+    const divNode = document.createElement('div');
+    const spanNode = document.createElement('span');
+    var service = document.createTextNode(index);
+    spanNode.appendChild(service);
+    divNode.appendChild(spanNode);
+    serviceOptionsNode.appendChild(divNode);
+}
