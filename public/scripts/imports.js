cd39b954bb22e690adb27ec8a8d730079e531c10
SilverSong3 <78618783+SilverSong3@users.noreply.github.com>
2023-04-09 23:55:03 -0400

    Made home the logo in nav menu and adjusted query

--- public/scripts/imports.js
+++ public/scripts/imports.js
@@ -23,11 +23,11 @@ window.onload = function loadMenu() {
         var Menu = document.getElementsByClassName("mainNav")[0];
     }
     //add all links to site as needed
-    Menu.innerHTML = "<a href=\"index.html\">Home</a>";
-    Menu.innerHTML += "<a href=\"services.html\">Services</a>";
-    Menu.innerHTML += "<a href=\"about.html\">About</a>";
-    Menu.innerHTML += "<a href=\"costestimator.html\">Cost Estimator</a>";
-    Menu.innerHTML += "<a href=\"contact.html\">Contact</a>";
+    Menu.innerHTML = "<a href=\"index.html\"><img id=\"headHomeLogo\" src=\"images/favicon.svg\" alt=\"WebDev Home Page header Favicon\"></a>";
+    Menu.innerHTML += "<a id=\"topNavBarLink\" href=\"services.html\">Services</a>";
+    Menu.innerHTML += "<a id=\"topNavBarLink\" href=\"about.html\">About</a>";
+    Menu.innerHTML += "<a id=\"topNavBarLink\" href=\"costestimator.html\">Cost Estimator</a>";
+    Menu.innerHTML += "<a id=\"topNavBarLink\" href=\"contact.html\">Contact</a>";
 
     //determine if current page is the logged in page
     if(document.getElementsByClassName('loggedInPage').length >= 1)
@@ -53,10 +53,10 @@ window.onload = function loadMenu() {
 
     //working on centering footer better... styled with footer img
     //in main.css
-    Footer.innerHTML = "<p>Copyright " + currentYear + "</p>";
-    Footer.innerHTML += "<a href=\"index.html\"><img src=\"images/favicon.svg\" alt=\"favicon\"></a>";
+    Footer.innerHTML = "<p>Copyright " + currentYear + " </p>";
+    Footer.innerHTML += "<a href=\"index.html\"><img src=\"images/favicon.svg\" alt=\"WebDev Home Page footer favicon\"></a>";
     Footer.innerHTML += "<p><a href=\"index.html\">WebDevs</a></p>";
     Footer.innerHTML += "<p id=\"footDetails\"><a href=\"about.html\">Developed by Team 1</a></p>";
-    Footer.innerHTML += "<p><a href=\"contact.html\">Send Us a Contact Email</a></p>";
+    Footer.innerHTML += "<p><a href=\"contact.html\">Send Us an Email</a></p>";
 }
 //NOTE: The " character must be escaped for the script to work properly.
